39719ec771563e0abd82f6b628d0fe3f852e3c11
Jonas Otto <jottosmail@gmail.com>
2020-04-03 16:23:40 +0200

    add missing values for GadgetEnum serialization

--- src/GadgetEnum.hpp
+++ src/GadgetEnum.hpp
@@ -34,9 +34,27 @@ namespace spy::gadget {
 
     NLOHMANN_JSON_SERIALIZE_ENUM(GadgetEnum, {
         { GadgetEnum::INVALID, nullptr },
+        { GadgetEnum::HAIRDRYER, "HAIRDRYER" },
+        { GadgetEnum::MOLEDIE, "MOLEDIE" },
+        { GadgetEnum::TECHNICOLOUR_PRISM, "TECHNICOLOUR_PRISM" },
+        { GadgetEnum::BOWLER_BLADE, "BOWLER_BLADE" },
+        { GadgetEnum::MAGNETIC_WATCH, "MAGNETIC_WATCH" },
+        { GadgetEnum::POISON_PILLS, "POISON_PILLS" },
+        { GadgetEnum::LASER_COMPACT, "LASER_COMPACT" },
         { GadgetEnum::ROCKET_PEN, "ROCKET_PEN" },
+        { GadgetEnum::GAS_GLOSS, "GAS_GLOSS" },
+        { GadgetEnum::MOTHBALL_POUCH, "MOTHBALL_POUCH" },
+        { GadgetEnum::FOG_TIN, "FOG_TIN" },
+        { GadgetEnum::GRAPPLE, "GRAPPLE" },
+        { GadgetEnum::WIRETAP_WITH_EARPLUGS, "WIRETAP_WITH_EARPLUGS" },
+        { GadgetEnum::DIAMOND_COLLAR, "DIAMOND_COLLAR" },
+        { GadgetEnum::JETPACK, "JETPACK" },
+        { GadgetEnum::CHICKEN_FEED, "CHICKEN_FEED" },
+        { GadgetEnum::NUGGET, "NUGGET" },
+        { GadgetEnum::MIRROR_OF_WILDERNESS, "MIRROR_OF_WILDERNESS" },
+        { GadgetEnum::POCKET_LITTER, "POCKET_LITTER" },
+        { GadgetEnum::COCKTAIL, "COCKTAIL" },
     })
-
 }
 
 #endif //LIBCOMMON_GADGETENUM_HPP
