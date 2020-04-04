f95744141a82c422bb18a964525b24ed0c62a8ea
Dominik Authaler <dominik.authaler@uni-ulm.de>
2020-04-04 19:31:10 +0200

    doxygen for character information class

--- src/datatypes/character/CharacterInformation.cpp
+++ src/datatypes/character/CharacterInformation.cpp
@@ -1,10 +1,18 @@
-//
-// Created by Carolin on 02.04.2020.
-//
+/**
+ * @file   CharacterInformation.cpp
+ * @author Carolin
+ * @date   02.04.2020 (creation)
+ * @brief  Definition of the character information class.
+ */
 
 #include "CharacterInformation.hpp"
 
 namespace spy::character {
+
+    CharacterInformation::CharacterInformation(util::UUID &characterId, CharacterDescription &character) :
+        characterId(characterId), character(character) {
+
+    }
 
     const util::UUID &CharacterInformation::getCharacterId() const {
         return characterId;
--- src/datatypes/character/CharacterInformation.hpp
+++ src/datatypes/character/CharacterInformation.hpp
@@ -1,9 +1,12 @@
-//
-// Created by Carolin on 02.04.2020.
-//
+/**
+ * @file   CharacterInformation.hpp
+ * @author Carolin
+ * @date   02.04.2020 (creation)
+ * @brief  Declaration of the character information class.
+ */
 
-#ifndef LIBCOMMON_CHARACTERINFORMATION_HPP
-#define LIBCOMMON_CHARACTERINFORMATION_HPP
+#ifndef LIBCOMMON_CHARACTER_INFORMATION_HPP
+#define LIBCOMMON_CHARACTER_INFORMATION_HPP
 
 #include <util/UUID.hpp>
 #include <string>
@@ -12,21 +15,36 @@
 #include <datatypes/character/CharacterDescription.hpp>
 
 namespace spy::character {
+    /**
+     * @brief Representation of the information of characters being used in the network protocol.
+     */
     class CharacterInformation {
-    public:
-        CharacterInformation(util::UUID &character_id, CharacterDescription &character) : characterId(
-                character_id), character(character) {}
+        public:
+            /**
+             * Constructor.
+             * @param characterId Id of the character.
+             * @param character   Character meta data.
+             */
+            CharacterInformation(util::UUID &characterId, CharacterDescription &character);
 
-        [[nodiscard]] const util::UUID &getCharacterId() const;
+            /**
+             * Getter for the character Id.
+             * @return Character Id.
+             */
+            [[nodiscard]] const util::UUID &getCharacterId() const;
 
-        [[nodiscard]] const CharacterDescription &getCharacter() const;
+            /**
+             * Getter for the character meta data.
+             * @return Character description (meta data).
+             */
+            [[nodiscard]] const CharacterDescription &getCharacter() const;
 
 
-    private:
-        const spy::util::UUID characterId;
-        const CharacterDescription character;
+        private:
+            const spy::util::UUID characterId;                  ///< Character Id.
+            const CharacterDescription character;               ///< Character meta data.
     };
 
 }
 
-#endif //LIBCOMMON_CHARACTERINFORMATION_HPP
+#endif //LIBCOMMON_CHARACTER_INFORMATION_HPP
