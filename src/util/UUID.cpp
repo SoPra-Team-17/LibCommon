4fe3f8107b2425c519be24377f2f777a04063de0
Jonas Otto <jottosmail@gmail.com>
2020-04-07 21:23:28 +0200

    libuuid wrapper

--- src/util/UUID.cpp
+++ src/util/UUID.cpp
@@ -3,3 +3,55 @@
 //
 
 #include "UUID.hpp"
+#include <array>
+
+namespace spy::util {
+
+    void UUID::clear() {
+        uuid_clear(uuidData);
+    }
+
+    std::string UUID::toString() const {
+        char uuidRawString[37];
+        uuid_unparse(uuidData, uuidRawString);
+        return std::string(uuidRawString);
+    }
+
+    UUID::UUID() {
+        uuid_generate(uuidData);
+    }
+
+    bool UUID::operator==(const UUID &rhs) const {
+        return uuid_compare(uuidData, rhs.uuidData) == 0;
+    }
+
+    bool UUID::operator<(const UUID &rhs) const {
+        return uuid_compare(uuidData, rhs.uuidData) < 0;
+    }
+
+    bool UUID::operator>(const UUID &rhs) const {
+        return rhs < *this;
+    }
+
+    bool UUID::operator<=(const UUID &rhs) const {
+        return !(rhs < *this);
+    }
+
+    bool UUID::operator>=(const UUID &rhs) const {
+        return !(*this < rhs);
+    }
+
+    UUID::UUID(const UUID &rhs) {
+        uuid_copy(uuidData, rhs.uuidData);
+    }
+
+    UUID::UUID(const std::string &s) {
+        if (uuid_parse(s.c_str(), uuidData) != 0) {
+            throw std::invalid_argument("Could not parse \"" + s + "\" as a UUID.");
+        }
+    }
+
+    std::ostream &operator<<(std::ostream &stream, const UUID &uuid) {
+        return stream << uuid.toString();
+    }
+}
--- src/util/UUID.hpp
+++ src/util/UUID.hpp
@@ -5,10 +5,40 @@
 #ifndef LIBCOMMON_UUID_HPP
 #define LIBCOMMON_UUID_HPP
 
+
+#include <uuid/uuid.h>
+#include <string>
+
 namespace spy::util {
+
     class UUID {
-        // TODO implement UUID class
+        public:
+            UUID();
+
+            UUID(const UUID &rhs);
+
+            UUID(const std::string &s);
+
+            void clear();
+
+            [[nodiscard]] std::string toString() const;
+
+            bool operator==(const UUID &rhs) const;
+
+            bool operator<(const UUID &rhs) const;
+
+            bool operator>(const UUID &rhs) const;
+
+            bool operator<=(const UUID &rhs) const;
+
+            bool operator>=(const UUID &rhs) const;
+
+            friend std::ostream &operator<<(std::ostream &stream, const UUID &uuid);
+
+        private:
+            uuid_t uuidData;
     };
+
 }
 
 #endif //LIBCOMMON_UUID_HPP
