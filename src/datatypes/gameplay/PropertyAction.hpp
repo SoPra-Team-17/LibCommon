1d1d3594840de88ea1384c66e0d970387bea2429
Jonas Otto <jottosmail@gmail.com>
2020-04-10 23:09:11 +0200

    add PropertyAction

---
+++ src/datatypes/gameplay/PropertyAction.hpp
@@ -0,0 +1,33 @@
+//
+// Created by jonas on 10.04.20.
+//
+
+#ifndef LIBCOMMON_PROPERTYACTION_HPP
+#define LIBCOMMON_PROPERTYACTION_HPP
+
+#include "Operation.hpp"
+#include <datatypes/character/PropertyEnum.hpp>
+
+namespace spy::gameplay {
+    class PropertyAction : public Operation {
+        public:
+            PropertyAction() = default;
+
+            PropertyAction(bool successful, const util::Point &target, const util::UUID &characterId,
+                           character::PropertyEnum usedProperty);
+
+            [[nodiscard]] character::PropertyEnum getUsedProperty() const;
+
+            bool operator==(const PropertyAction &rhs) const;
+
+            bool operator!=(const PropertyAction &rhs) const;
+
+            friend void to_json(nlohmann::json &j, const PropertyAction &p);
+
+            friend void from_json(const nlohmann::json &j, PropertyAction &p);
+
+        private:
+            character::PropertyEnum usedProperty;
+    };
+}
+#endif //LIBCOMMON_PROPERTYACTION_HPP
