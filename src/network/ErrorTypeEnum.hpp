1fd554c92e03eb906a0f2099373ffc550507cb76
Jonas Otto <jottosmail@gmail.com>
2020-04-10 02:40:49 +0200

    add new error types

--- src/network/ErrorTypeEnum.hpp
+++ src/network/ErrorTypeEnum.hpp
@@ -9,17 +9,22 @@
 
 namespace spy::network {
     enum class ErrorTypeEnum {
+            // TODO: handle special INVALID case (consistently among enums?)
             INVALID,
             NAME_NOT_AVAILABLE,
             ALREADY_SERVING,
-            GENERAL
+            SESSION_DOES_NOT_EXIST,
+            ILLEGAL_MESSAGE,
+            TOO_MANY_STRIKES,
     };
 
     NLOHMANN_JSON_SERIALIZE_ENUM(ErrorTypeEnum, {
         { ErrorTypeEnum::INVALID, nullptr },
         { ErrorTypeEnum::NAME_NOT_AVAILABLE, "NAME_NOT_AVAILABLE" },
         { ErrorTypeEnum::ALREADY_SERVING, "ALREADY_SERVING" },
-        { ErrorTypeEnum::GENERAL, "GENERAL" }
+        { ErrorTypeEnum::SESSION_DOES_NOT_EXIST, "SESSION_DOES_NOT_EXIST" },
+        { ErrorTypeEnum::ILLEGAL_MESSAGE, "ILLEGAL_MESSAGE" },
+        { ErrorTypeEnum::TOO_MANY_STRIKES, "TOO_MANY_STRIKES" }
     })
 }
 
