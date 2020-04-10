e4ebc276bdefa92329450b6cbb152953edbc127c
marco5de <marco.deuscher@t-online.de>
2020-04-10 13:54:07 +0200

    request replay message

---
+++ src/network/messages/RequestReplay.hpp
@@ -0,0 +1,26 @@
+//
+// Created by marco on 10.04.20.
+//
+
+#ifndef LIBCOMMON_REQUESTREPLAY_HPP
+#define LIBCOMMON_REQUESTREPLAY_HPP
+
+#include <network/MessageContainer.hpp>
+
+namespace spy::network::messages {
+
+    class RequestReplay : public MessageContainer {
+        public:
+            RequestReplay();
+
+            RequestReplay(const util::UUID &playerId);
+
+            friend void to_json(nlohmann::json &j, const RequestReplay &r);
+
+            friend void from_json(const nlohmann::json &j, RequestReplay &r);
+    };
+
+}
+
+
+#endif //LIBCOMMON_REQUESTREPLAY_HPP
