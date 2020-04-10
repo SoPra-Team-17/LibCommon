a05c595e65ad81c41beada39e23715a734f5a318
marco5de <marco.deuscher@t-online.de>
2020-04-10 13:35:13 +0200

    Request Meta Information message

---
+++ src/network/messages/RequestMetaInformation.hpp
@@ -0,0 +1,31 @@
+//
+// Created by marco on 10.04.20.
+//
+
+#ifndef LIBCOMMON_REQUESTMETAINFORMATION_HPP
+#define LIBCOMMON_REQUESTMETAINFORMATION_HPP
+
+#include <string>
+#include <network/MessageContainer.hpp>
+
+namespace spy::network::messages {
+
+    class RequestMetaInformation : public MessageContainer {
+        public:
+            RequestMetaInformation();
+
+            RequestMetaInformation(const util::UUID &playerId, const std::vector<std::string> &keys);
+
+            [[nodiscard]] const std::vector<std::string> &getKeys() const;
+
+            friend void to_json(nlohmann::json &j, const RequestMetaInformation &r);
+
+            friend void from_json(const nlohmann::json &j, RequestMetaInformation &r);
+
+        private:
+            std::vector<std::string> keys;
+    };
+
+}
+
+#endif //LIBCOMMON_REQUESTMETAINFORMATION_HPP
