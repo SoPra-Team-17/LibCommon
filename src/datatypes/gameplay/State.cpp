585ca3dd0a1159d347488fd86f96ec7ed0e81fdf
marco5de <marco.deuscher@t-online.de>
2020-04-10 13:13:15 +0200

    game status  message

---
+++ src/datatypes/gameplay/State.cpp
@@ -0,0 +1,15 @@
+//
+// Created by marco on 10.04.20.
+//
+
+#include "State.hpp"
+
+namespace spy::gameplay {
+    void to_json(nlohmann::json &/*j*/, const State &/*s*/) {
+
+    }
+
+    void from_json(const nlohmann::json &/*j*/, State &/*s*/) {
+
+    }
+}
