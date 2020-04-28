e91f2df8ff7f30f4a9a6b47eeeefd5df95c7eeef
Jonas Otto <jottosmail@gmail.com>
2020-04-28 17:41:59 +0200

    fix code smells: remove empty statement, explicitly capture variable in lambda

--- src/datatypes/execution/gadget/HairDryer.cpp
+++ src/datatypes/execution/gadget/HairDryer.cpp
@@ -7,8 +7,8 @@ namespace spy::gameplay {
     bool GadgetExecutor::executeHairDryer(State &s, const GadgetAction &action) {
         // Search character at target location
         auto charTarget = std::find_if(s.getCharacters().begin(), s.getCharacters().end(),
-                                       [&](const character::Character &c) {
-                                           return c.getCoordinates() == action.getTarget();;
+                                       [&action](const character::Character &c) {
+                                           return c.getCoordinates() == action.getTarget();
                                        });
 
         // Remove CLAMMY_CLOTHES (Lastenheft: "Klamme Klamotten") property
