1454845f9feac45bd487b8bed4502a82e608e61d
Dominik Authaler <dominik.authaler@uni-ulm.de>
2020-04-29 13:59:35 +0200

    add hairdryer validation

---
+++ src/datatypes/validation/gadget/Hairdryer.cpp
@@ -0,0 +1,24 @@
+/**
+* @file   Hairdryer.cpp
+* @author Dominik Authaler
+* @date   29.04.2020 (creation)
+* @brief  Implementation of hairdryer gadget validation.
+*/
+
+namespace spy::gameplay {
+    bool GadgetValidator::validateHairdryer(const State &s, GadgetAction a) {
+        // check range
+        if (Movement::getMoveDistance(character->getCoordinates().value(), op.getTarget()) > 1) {
+            return false;
+        }
+
+        // search for character at target position
+        auto charTarget = std::find_if(characters.begin(), characters.end(),
+                                       [&op](const character::Character &c) {
+                                           return c.getCoordinates() == op.getTarget();
+                                       });
+
+        return (charTarget == characters.end());
+    }
+}
+
