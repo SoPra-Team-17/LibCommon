8dd966c9b3ae3474c232c6a021c08ef558730687
Dominik Authaler <dominik.authaler@uni-ulm.de>
2020-04-29 22:55:18 +0200

    added target outside test for prism

--- test/datatypes/validation/gadget/technicolourPrismTests.cpp
+++ test/datatypes/validation/gadget/technicolourPrismTests.cpp
@@ -19,6 +19,7 @@ TEST_F(GadgetActionTests, TechnicolourPrism_Validate) {
     state.getCharacters().getByUUID(uuid1)->setCoordinates({4, 3});
     state.getCharacters().getByUUID(uuid2)->setCoordinates({5, 3});
     state.getCharacters().getByUUID(uuid3)->setCoordinates({3, 2});
+    state.getCharacters().getByUUID(uuid5)->setCoordinates({6, 6});
 
     state.getCharacters().getByUUID(uuid1)->addGadget(prism);
     state.getCharacters().getByUUID(uuid5)->addGadget(prism);
@@ -27,11 +28,13 @@ TEST_F(GadgetActionTests, TechnicolourPrism_Validate) {
     GadgetAction g2(false, {3, 3}, uuid2, GadgetEnum::TECHNICOLOUR_PRISM);
     GadgetAction g3(false, {4, 2}, uuid1, GadgetEnum::TECHNICOLOUR_PRISM);
     GadgetAction g4(false, {3, 3}, uuid3, GadgetEnum::TECHNICOLOUR_PRISM);
+    GadgetAction g5(false, {6, 7}, uuid5, GadgetEnum::TECHNICOLOUR_PRISM);
 
     EXPECT_TRUE(ActionValidator::validate(state, g1));      // roulette table out of range --> invalid
     EXPECT_FALSE(ActionValidator::validate(state, g2));     // roulette table is target in range --> valid
     EXPECT_FALSE(ActionValidator::validate(state, g3));     // target is no roulette table --> invalid
     EXPECT_FALSE(ActionValidator::validate(state, g4));     // character has no prism --> invalid
+    EXPECT_FALSE(ActionValidator::validate(state, g5));     // target field is outside --> invalid
 }
 
 
