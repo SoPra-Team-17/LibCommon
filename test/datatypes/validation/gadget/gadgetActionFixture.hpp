90fd594ca24943a81d7afd67c6033bd8924f3c8a
Dominik Authaler <dominik.authaler@uni-ulm.de>
2020-04-29 22:28:04 +0200

    added test for hairdryer validation

---
+++ test/datatypes/validation/gadget/gadgetActionFixture.hpp
@@ -0,0 +1,46 @@
+/**
+ * @file   gadgetActionFixture.hpp
+ * @author Dominik Authaler
+ * @date   29.04.2020 (creation)
+ * @brief  Fixture for the gadget tests.
+ */
+
+#ifndef LIBCOMMON_GADGET_ACTION_FIXTURE_HPP
+#define LIBCOMMON_GADGET_ACTION_FIXTURE_HPP
+
+#include "scenario/Scenario.hpp"
+#include "scenario/FieldMap.hpp"
+#include "gameplay/State.hpp"
+#include "character/Character.hpp"
+#include "util/Point.hpp"
+#include "datatypes/validation/ActionValidator.hpp"
+
+class GadgetActionTests : public ::testing::Test {
+    protected:
+        spy::util::UUID uuid1 = spy::util::UUID::generate();
+        spy::util::UUID uuid2 = spy::util::UUID::generate();
+        spy::util::UUID uuid3 = spy::util::UUID::generate();
+        spy::util::UUID uuid4 = spy::util::UUID::generate();
+        spy::util::UUID uuid5 = spy::util::UUID::generate();
+
+        nlohmann::json input = R"({ "scenario": [
+            ["WALL", "WALL",      "WALL", "WALL",           "WALL",     "WALL", "WALL"],
+            ["WALL", "FIREPLACE", "WALL", "BAR_TABLE",      "BAR_SEAT", "FREE", "WALL"],
+            ["WALL", "FREE",      "FREE", "FREE",           "FREE",     "FREE", "WALL"],
+            ["WALL", "BAR_TABLE", "WALL", "ROULETTE_TABLE", "FREE",     "FREE", "WALL"],
+            ["WALL", "BAR_SEAT",  "FREE", "WALL",           "FREE",     "FREE", "WALL"],
+            ["WALL", "FREE",      "FREE", "FREE",           "FREE",     "SAFE", "WALL"],
+            ["WALL", "WALL",      "WALL", "WALL",           "WALL",     "WALL", "FREE"]
+            ]})"_json;
+
+        spy::scenario::Scenario decodedScenario = input.get<spy::scenario::Scenario>();
+
+        spy::scenario::FieldMap field{decodedScenario};
+
+        spy::character::CharacterSet characters = {{uuid1, "dummy"}, {uuid2, "dummy"}, {uuid3, "dummy"},
+                                                   {uuid4, "dummy"}, {uuid5, "dummy"}};
+
+        spy::gameplay::State state{0, field, {}, characters, std::nullopt, std::nullopt};
+};
+
+#endif //LIBCOMMON_GADGET_ACTION_FIXTURE_HPP
---
+++ test/datatypes/validation/gadget/hairdryerTests.cpp
@@ -0,0 +1,55 @@
+/**
+ * @file   hairdryerTests.cpp
+ * @author Dominik Authaler
+ * @date   29.04.2020 (creation)
+ * @brief  Unit tests for the validation and execution of hairdryer gadget actions.
+ */
+
+#include <gtest/gtest.h>
+#include "gadgetActionFixture.hpp"
+
+TEST_F(GadgetActionTests, Hairdryer_Validate) {
+    using spy::gameplay::ActionValidator;
+    using spy::gadget::GadgetEnum;
+    using spy::character::PropertyEnum;
+    using spy::util::Point;
+    using spy::gameplay::GadgetAction;
+
+    spy::gadget::Gadget hairdryer{spy::gadget::GadgetEnum::HAIRDRYER};
+
+    state.getCharacters().getByUUID(uuid1)->setCoordinates({4, 3});
+    state.getCharacters().getByUUID(uuid2)->setCoordinates({4, 2});
+    state.getCharacters().getByUUID(uuid3)->setCoordinates({4, 4});
+    state.getCharacters().getByUUID(uuid4)->setCoordinates({5, 3});
+    state.getCharacters().getByUUID(uuid5)->setCoordinates({6, 6});
+
+    state.getCharacters().getByUUID(uuid1)->addGadget(hairdryer);
+    state.getCharacters().getByUUID(uuid5)->addGadget(hairdryer);
+
+    GadgetAction g1(false, {4, 3}, uuid2, GadgetEnum::HAIRDRYER);
+    GadgetAction g2(false, {6, 7}, uuid5, GadgetEnum::HAIRDRYER);
+    GadgetAction g3(false, {4, 3}, uuid1, GadgetEnum::HAIRDRYER);
+    GadgetAction g4(false, {4, 3}, uuid1, GadgetEnum::HAIRDRYER);
+    GadgetAction g5(false, {2, 3}, uuid1, GadgetEnum::HAIRDRYER);
+    GadgetAction g6(false, {3, 3}, uuid1, GadgetEnum::HAIRDRYER);
+    GadgetAction g7(false, {4, 2}, uuid1, GadgetEnum::HAIRDRYER);
+    GadgetAction g8(false, {4, 4}, uuid1, GadgetEnum::HAIRDRYER);
+
+    EXPECT_FALSE(ActionValidator::validate(state, g1));     // execution without owning a hairdryer is invalid
+    EXPECT_FALSE(ActionValidator::validate(state, g2));     // execution on a field outside is invalid
+    EXPECT_TRUE(ActionValidator::validate(state, g3));      // execution on oneself without CLAMMY_CLOTHES is valid
+
+    std::vector<PropertyEnum> properties = {PropertyEnum::CLAMMY_CLOTHES};
+    state.getCharacters().getByUUID(uuid5)->setProperties(properties);
+    state.getCharacters().getByUUID(uuid3)->setProperties(properties);
+
+    EXPECT_TRUE(ActionValidator::validate(state, g4));      // execution on oneself with CLAMMY_CLOTHES is valid
+    EXPECT_FALSE(ActionValidator::validate(state, g5));     // target out of range --> invalid
+
+    EXPECT_FALSE(ActionValidator::validate(state, g6));     // execution on empty field is invalid
+    EXPECT_TRUE(ActionValidator::validate(state, g7));      // execution on character without CLAMMY_CLOTHES is valid
+    EXPECT_TRUE(ActionValidator::validate(state, g8));      // execution on character with CLAMMY_CLOTHES is valid
+}
+
+
+
