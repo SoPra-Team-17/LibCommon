2c4f8b7342b199de97518f02cf1e059280721639
Dominik Authaler <dominik.authaler@uni-ulm.de>
2020-04-30 21:37:21 +0200

    add target out of range testcase for cocktail tests

--- test/gameLogic/gadget/cocktailTests.cpp
+++ test/gameLogic/gadget/cocktailTests.cpp
@@ -20,6 +20,7 @@ TEST_F(GadgetActionTests, Cocktail_Validate) {
     state.getCharacters().getByUUID(uuid1)->setCoordinates({4, 3});
     state.getCharacters().getByUUID(uuid2)->setCoordinates({5, 3});
     state.getCharacters().getByUUID(uuid3)->setCoordinates({4, 2});
+    state.getCharacters().getByUUID(uuid4)->setCoordinates({4, 5});
     state.getCharacters().getByUUID(uuid5)->setCoordinates({6, 6});
 
     state.getCharacters().getByUUID(uuid1)->addGadget(cocktail);
@@ -30,12 +31,14 @@ TEST_F(GadgetActionTests, Cocktail_Validate) {
     GadgetAction g3(false, {4, 4}, uuid1, GadgetEnum::COCKTAIL);
     GadgetAction g4(false, {4, 2}, uuid1, GadgetEnum::COCKTAIL);
     GadgetAction g5(false, {6, 7}, uuid5, GadgetEnum::COCKTAIL);
+    GadgetAction g6(false, {4, 5}, uuid1, GadgetEnum::COCKTAIL);
 
     EXPECT_FALSE(ActionValidator::validate(state, g1));     // character has no cocktail --> invalid
     EXPECT_TRUE(ActionValidator::validate(state, g2));      // drink cocktail --> valid
     EXPECT_FALSE(ActionValidator::validate(state, g3));     // spill on empty field --> invalid
     EXPECT_TRUE(ActionValidator::validate(state, g4));      // spill on character --> valid
     EXPECT_FALSE(ActionValidator::validate(state, g5));     // target outside map --> invalid
+    EXPECT_FALSE(ActionValidator::validate(state, g6));     // target out of range --> invalid
 }
 
 
