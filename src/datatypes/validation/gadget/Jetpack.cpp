0524493dab74ecb3da7edccaa5249979035e6650
Dominik Authaler <dominik.authaler@uni-ulm.de>
2020-04-30 21:26:27 +0200

    fixed implementation of jetpack validation

--- src/datatypes/validation/gadget/Jetpack.cpp
+++ src/datatypes/validation/gadget/Jetpack.cpp
@@ -10,8 +10,22 @@
 namespace spy::gameplay {
 
     bool GadgetValidator::validateJetpack(const State &s, GadgetAction a) {
-        // check if target field is free
-        return s.getMap().getField(a.getTarget()).getFieldState() == scenario::FieldStateEnum::FREE;
+        // flying to the field the character is currently standing on is useless, but valid
+        if (s.getCharacters().findByUUID(a.getCharacterId().value())->getCoordinates() == a.getTarget()) {
+            return true;
+        }
+
+        // check if target field type is free
+        if (s.getMap().getField(a.getTarget()).getFieldState() != scenario::FieldStateEnum::FREE) {
+            return false;
+        }
+
+        //check if any character is occupying the field
+        auto person = std::find_if(s.getCharacters().begin(), s.getCharacters().end(),
+                                   [&a](const character::Character &c) {
+                                       return c.getCoordinates() == a.getTarget();
+                                   });
+        return (person == s.getCharacters().end());
     }
 
 }
