a828f126eb889366d65271aadbfbcf1b91a6e332
csacro <50425802+csacro@users.noreply.github.com>
2020-05-02 23:15:35 +0200

    implemented executeExfiltration

--- src/gameLogic/execution/ActionExecutor_Gadget.cpp
+++ src/gameLogic/execution/ActionExecutor_Gadget.cpp
@@ -8,4 +8,9 @@ namespace spy::gameplay {
     bool ActionExecutor::executeGadget(State &s, const GadgetAction &op, const MatchConfig &config) {
         return GadgetExecutor::execute(s, op, config);
     }
+
+    const util::Point &ActionExecutor::getRandomFreeNeighbouringField(const util::Point &point) {
+        // TODO implement
+        return point;
+    }
 }
