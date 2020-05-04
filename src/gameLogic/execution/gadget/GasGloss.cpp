61cd7d6565f8fe72dbe96ce2126d9378cbd70490
csacro <50425802+csacro@users.noreply.github.com>
2020-05-04 17:52:57 +0200

    implement #108

---
+++ src/gameLogic/execution/gadget/GasGloss.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Carolin on 04.05.2020.
+//
+
+#include <util/GameLogicUtils.hpp>
+#include "GadgetExecutor.hpp"
+
+namespace spy::gameplay {
+
+    bool GadgetExecutor::executeGasGloss(State &s, const GadgetAction &a, const MatchConfig &config) {
+        auto character = *(s.getCharacters().getByUUID(a.getCharacterId()));
+
+        // Babysitter successfull
+        bool babysitter = util::GameLogicUtils::checkBabySitter(s, a, config);
+
+        auto targetPerson = util::GameLogicUtils::getInCharacterSetByCoordinates(s.getCharacters(), a.getTarget());
+
+        if (!babysitter) {
+            auto damage = config.getGasGlossDamage();
+            targetPerson->subHealthPoints(damage);
+        }
+
+        // remove gas gloss from inventory
+        character.removeGadget(gadget::GadgetEnum::GAS_GLOSS);
+
+        return true;
+    }
+
+}
