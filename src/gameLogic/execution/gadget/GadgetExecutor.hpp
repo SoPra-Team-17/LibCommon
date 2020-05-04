5bf4accbfdc5fcab30c0b4515f11a69299d73acf
Marco Deuscher <33183479+Marco5de@users.noreply.github.com>
2020-05-04 22:41:56 +0200

    inital mow execution (#190)

--- src/gameLogic/execution/gadget/GadgetExecutor.hpp
+++ src/gameLogic/execution/gadget/GadgetExecutor.hpp
@@ -33,12 +33,13 @@ namespace spy::gameplay {
 
             static bool executeBowlerBlade(State &s, const GadgetAction &a, const MatchConfig &config);
 
+            static bool executeMirrorOfWilderness(State &s, const GadgetAction &a, const MatchConfig &config);
+
             static bool executeNugget(State &s, const GadgetAction &a);
 
             static bool executeRocketPen(State &s, const GadgetAction &a, const MatchConfig &config);
 
             static bool executeMoleDie(State &s, const GadgetAction &a);
-
 
     };
 }
