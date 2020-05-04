a542f8802e0e5a5ba2ecfc7b6f6c731d13722b9e
csacro <50425802+csacro@users.noreply.github.com>
2020-05-04 12:51:04 +0200

    return nullptr for unimplemented methods
    Co-authored-by: Jonas Otto <jottosmail@gmail.com>

--- src/gameLogic/generation/ActionGenerator_Server.cpp
+++ src/gameLogic/generation/ActionGenerator_Server.cpp
@@ -11,7 +11,7 @@ namespace spy::gameplay {
     std::shared_ptr<BaseOperation>
     ActionGenerator::generateExfiltration(const State &/*s*/, const util::UUID &/*damagedCharacter*/) {
         // TODO implement
-        return std::shared_ptr<BaseOperation>();
+        return nullptr;
     }
 
     std::shared_ptr<BaseOperation> ActionGenerator::generateCatAction(const State &s) {
