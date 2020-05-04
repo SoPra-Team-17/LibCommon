e97079fb00da0bd8ee15a6a9a4d227c9378496e2
marco5de <marco.deuscher@t-online.de>
2020-05-04 11:24:21 +0200

    Revert "modified spy action validator for safe picking"
    This reverts commit b78cf656

--- src/gameLogic/validation/ActionValidator_Spy.cpp
+++ src/gameLogic/validation/ActionValidator_Spy.cpp
@@ -7,24 +7,8 @@
 
 namespace spy::gameplay {
 
-    bool ActionValidator::validateSpyAction(const State &s, const spy::gameplay::SpyAction &op) {
-        using spy::character::PropertyEnum;
-
+    bool ActionValidator::validateSpyAction(const State &s, const spy::gameplay::SpyAction& op) {
         auto character = s.getCharacters().findByUUID(op.getCharacterId());
-        bool spyOnPerson = spy::util::GameLogicUtils::personOnNeighboringField(s, op.getTarget(),
-                                                                               character->getCoordinates().value());
-        if (spyOnPerson) {
-            return true;
-        }
-
-        // check if target field has safe
-        bool hasSafe = s.getMap().getField(op.getTarget()).getFieldState() == scenario::FieldStateEnum::SAFE;
-        // check distance to field
-        auto distance = Movement::getMoveDistance(op.getTarget(), character->getCoordinates().value());
-        if (hasSafe && distance == 1) {
-            return true;
-        }
-
-        return hasSafe && (distance == 2) && character->hasProperty(character::PropertyEnum::FLAPS_AND_SEALS);
+        return spy::util::GameLogicUtils::personOnNeighboringField(s, op.getTarget(), character->getCoordinates().value());
     }
 }
