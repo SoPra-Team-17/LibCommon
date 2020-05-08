8c8490b72ab4686ac896c6cf33e83cb768ecb300
marco5de <marco.deuscher@t-online.de>
2020-05-08 16:57:45 +0200

    generate poison pills

--- src/gameLogic/generation/ActionGenerator.hpp
+++ src/gameLogic/generation/ActionGenerator.hpp
@@ -10,6 +10,7 @@
 
 #include <datatypes/gameplay/State.hpp>
 #include <util/UUID.hpp>
+#include <datatypes/matchconfig/MatchConfig.hpp>
 
 namespace spy::gameplay {
     /**
@@ -105,7 +106,7 @@ namespace spy::gameplay {
             generateNugget(const State &s, const util::UUID &activeCharacter);
 
             static std::vector<std::shared_ptr<BaseOperation>>
-            generatePoisonPills(const State &s, const util::UUID &activeCharacter);
+            generatePoisonPills(const State &s, const util::UUID &activeCharacter, const spy::MatchConfig &config);
 
             static std::vector<std::shared_ptr<BaseOperation>>
             generateRocketPen(const State &s, const util::UUID &activeCharacter);
