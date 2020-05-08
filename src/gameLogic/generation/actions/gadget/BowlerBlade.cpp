49800dd3434231a088bc08266ef4f1ea3925e7f8
marco5de <marco.deuscher@t-online.de>
2020-05-08 15:45:56 +0200

    sinnvollere implementierung

--- src/gameLogic/generation/actions/gadget/BowlerBlade.cpp
+++ src/gameLogic/generation/actions/gadget/BowlerBlade.cpp
@@ -3,12 +3,28 @@
 //
 
 #include "gameLogic/generation/ActionGenerator.hpp"
+#include "gameLogic/validation/ActionValidator.hpp"
+#include "util/GameLogicUtils.hpp"
+#include "datatypes/gameplay/GadgetAction.hpp"
 
 namespace spy::gameplay {
     std::vector<std::shared_ptr<BaseOperation>>
-    ActionGenerator::generateBowlerBlade(const State &/*s*/, const util::UUID &/*activeCharacter*/) {
+    ActionGenerator::generateBowlerBlade(const State &s, const util::UUID &activeCharacter,
+                                         const spy::MatchConfig &config) {
+        std::vector<std::shared_ptr<BaseOperation>> valid_ops;
 
-        // TODO implement
-        return {nullptr};
+        for (const auto &targetChar : s.getCharacters()) {
+            if (targetChar.getCoordinates().has_value()) {
+                auto action = std::make_shared<GadgetAction>(false, targetChar.getCoordinates().value(),
+                                                             activeCharacter,
+                                                             gadget::GadgetEnum::BOWLER_BLADE);
+                bool valid = ActionValidator::validate(s, action, config);
+                if (valid) {
+                    valid_ops.push_back(action);
+                }
+            }
+        }
+
+        return valid_ops;
     }
 }
