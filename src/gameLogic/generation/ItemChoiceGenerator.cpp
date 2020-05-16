643da67620e6be094e2277226184e42699558662
csacro <50425802+csacro@users.noreply.github.com>
2020-05-16 13:24:19 +0200

    implemented #224

--- src/gameLogic/generation/ItemChoiceGenerator.cpp
+++ src/gameLogic/generation/ItemChoiceGenerator.cpp
@@ -3,13 +3,30 @@
 //
 
 #include "ItemChoiceGenerator.hpp"
+#include "util/GameLogicUtils.hpp"
 
 namespace spy::gameplay {
 
     std::variant<util::UUID, gadget::GadgetEnum>
-    ItemChoiceGenerator::getRandomItemChoice(const std::vector<util::UUID>& /*offeredCharacterIds*/,
-                                             const std::vector<spy::gadget::GadgetEnum>& /*offeredGadgets*/) {
-        // TODO implement
-        return std::variant<util::UUID, gadget::GadgetEnum>();
+    ItemChoiceGenerator::getRandomItemChoice(const std::vector<util::UUID>& offeredCharacterIds,
+                                             const std::vector<spy::gadget::GadgetEnum>& offeredGadgets) {
+
+
+        if (!offeredCharacterIds.empty() && !offeredGadgets.empty()) {
+            auto character = util::GameLogicUtils::getRandomItemFromContainer(offeredCharacterIds);
+            auto gadget = util::GameLogicUtils::getRandomItemFromContainer(offeredGadgets);
+            auto r = util::GameLogicUtils::probabilityTest(0.5);
+            if (r) {
+                return std::variant<util::UUID, gadget::GadgetEnum>(*character);
+            } else {
+                return std::variant<util::UUID, gadget::GadgetEnum>(*gadget);
+            }
+        }
+        if(offeredCharacterIds.empty()) {
+            return std::variant<util::UUID, gadget::GadgetEnum>(*util::GameLogicUtils::getRandomItemFromContainer(offeredGadgets));
+        }
+        if (offeredGadgets.empty()) {
+            return std::variant<util::UUID, gadget::GadgetEnum>(*util::GameLogicUtils::getRandomItemFromContainer(offeredCharacterIds));
+        }
     }
 }
