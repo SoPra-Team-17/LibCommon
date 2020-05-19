fe6ec1169c3b499834e5bb58da3fb810831286ee
Dominik Authaler <dominik.authaler@uni-ulm.de>
2020-05-19 21:41:31 +0200

    added usagesLeft initialization according to type

--- src/datatypes/gadgets/Gadget.cpp
+++ src/datatypes/gadgets/Gadget.cpp
@@ -6,7 +6,60 @@
 
 namespace spy::gadget {
 
-    Gadget::Gadget(GadgetEnum type, int usagesLeft) : type(type), usagesLeft(usagesLeft) {}
+    Gadget::Gadget(GadgetEnum type) : type(type) {
+        switch (type) {
+            case GadgetEnum::HAIRDRYER:
+                [[fallthrough]];
+            case GadgetEnum::BOWLER_BLADE:
+                [[fallthrough]];
+            case GadgetEnum::MAGNETIC_WATCH:
+                [[fallthrough]];
+            case GadgetEnum::LASER_COMPACT:
+                [[fallthrough]];
+            case GadgetEnum::GRAPPLE:
+                [[fallthrough]];
+            case GadgetEnum::DIAMOND_COLLAR:
+                [[fallthrough]];
+            case GadgetEnum::POCKET_LITTER:
+                [[fallthrough]];
+            case GadgetEnum::MOLEDIE:
+                usagesLeft = std::nullopt;
+                break;
+
+
+            case GadgetEnum::NUGGET:
+                [[fallthrough]];
+            case GadgetEnum::TECHNICOLOUR_PRISM:
+                [[fallthrough]];
+            case GadgetEnum::ROCKET_PEN:
+                [[fallthrough]];
+            case GadgetEnum::GAS_GLOSS:
+                [[fallthrough]];
+            case GadgetEnum::WIRETAP_WITH_EARPLUGS:
+                [[fallthrough]];
+            case GadgetEnum::JETPACK:
+                [[fallthrough]];
+            case GadgetEnum::CHICKEN_FEED:
+                [[fallthrough]];
+            case GadgetEnum::COCKTAIL:
+                [[fallthrough]];
+            case GadgetEnum::FOG_TIN:
+                usagesLeft = 1;
+                break;
+
+            case GadgetEnum::MOTHBALL_POUCH:
+                [[fallthrough]];
+            case GadgetEnum::POISON_PILLS:
+                usagesLeft = 5;
+                break;
+
+            default:
+                usagesLeft = 0;
+                break;
+        }
+    }
+
+    Gadget::Gadget(GadgetEnum type, unsigned int usagesLeft) : type(type), usagesLeft(usagesLeft) {}
 
     GadgetEnum Gadget::getType() const {
         return type;
