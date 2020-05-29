fe25440085a060c352279fa9f9ed75c1fa2dced3
Dominik Authaler <dominik.authaler@uni-ulm.de>
2020-05-29 21:37:31 +0200

    fixed nugget tests

--- test/gameLogic/gadget/nuggetTests.cpp
+++ test/gameLogic/gadget/nuggetTests.cpp
@@ -22,13 +22,14 @@ TEST_F(GadgetActionTests, Nugget_Validate) {
     state.getCharacters().getByUUID(uuid2)->setCoordinates({5, 3});
     state.getCharacters().getByUUID(uuid2)->setFaction(spy::character::FactionEnum::PLAYER2);
     state.getCharacters().getByUUID(uuid3)->setCoordinates({4, 2});
+    state.getCharacters().getByUUID(uuid3)->setFaction(spy::character::FactionEnum::PLAYER1);
     state.getCharacters().getByUUID(uuid4)->setCoordinates({4, 5});
     state.getCharacters().getByUUID(uuid5)->setCoordinates({6, 6});
 
     state.getCharacters().getByUUID(uuid1)->addGadget(nugget);
-    state.getCharacters().getByUUID(uuid2)->setFaction(spy::character::FactionEnum::PLAYER1);
+    state.getCharacters().getByUUID(uuid1)->setFaction(spy::character::FactionEnum::PLAYER1);
     state.getCharacters().getByUUID(uuid5)->addGadget(nugget);
-    state.getCharacters().getByUUID(uuid2)->setFaction(spy::character::FactionEnum::PLAYER1);
+    state.getCharacters().getByUUID(uuid5)->setFaction(spy::character::FactionEnum::PLAYER1);
 
     auto g1 = std::make_shared<GadgetAction>(GadgetAction(false, {5, 3}, uuid2, GadgetEnum::NUGGET));
     auto g2 = std::make_shared<GadgetAction>(GadgetAction(false, {4, 3}, uuid1, GadgetEnum::NUGGET));
@@ -44,5 +45,5 @@ TEST_F(GadgetActionTests, Nugget_Validate) {
     EXPECT_FALSE(ActionValidator::validate(state, g4, config)) << "aim on character of own faction --> invalid";
     EXPECT_FALSE(ActionValidator::validate(state, g5, config)) << "target outside map --> invalid";
     EXPECT_FALSE(ActionValidator::validate(state, g6, config)) << "target outside range --> invalid";
-    EXPECT_TRUE(ActionValidator::validate(state, g7, config)) << "aim on character of other faction --> valid";
+    EXPECT_TRUE (ActionValidator::validate(state, g7, config)) << "aim on character of other faction --> valid";
 }
