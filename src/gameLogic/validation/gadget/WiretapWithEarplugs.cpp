dcf70c4a1d0eae9baa585c36204fe447f3316593
Dominik Authaler <dominik.authaler@uni-ulm.de>
2020-05-29 21:41:26 +0200

    fixed wiretap with earplugs on own faction

--- src/gameLogic/validation/gadget/WiretapWithEarplugs.cpp
+++ src/gameLogic/validation/gadget/WiretapWithEarplugs.cpp
@@ -15,10 +15,21 @@ namespace spy::gameplay {
         auto character = s.getCharacters().findByUUID(a.getCharacterId());
         auto gadget = std::dynamic_pointer_cast<const gadget::WiretapWithEarplugs>(
                 character->getGadget(a.getGadget()).value());
+
         if (gadget->getActiveOn().has_value()) {
             //gadget is already in use
             return false;
         }
-        return spy::util::GameLogicUtils::personOnNeighbourField(s, a.getTarget(), character->getCoordinates().value());
+
+        auto targetUUID = spy::util::GameLogicUtils::getPersonOnNeighbourField(s, a.getTarget(),
+                                                                               character->getCoordinates().value());
+        if (!targetUUID.has_value()) {
+            // no character on target field
+            return false;
+        } else {
+            // wiretap with earplugs can only be used against characters of another faction
+            auto targetChar = s.getCharacters().findByUUID(targetUUID.value());
+            return (targetChar->getFaction() != character->getFaction());
+        }
     }
 }
