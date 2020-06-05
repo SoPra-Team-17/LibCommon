d5d73caab0eef9a7c1062d3ae3230e9257a70f5e
Jonas Otto <jottosmail@gmail.com>
2020-06-05 22:23:46 +0200

    checkout fixed release of nlohmann/json

--- installDependencies.sh
+++ installDependencies.sh
@@ -9,10 +9,8 @@ sudo apt install -y uuid-dev
 
 # nlohmann json
 cd /tmp
-git clone --depth 1 --branch master https://github.com/nlohmann/json.git
+git clone --depth 1 -b v3.7.3 https://github.com/nlohmann/json.git
 cd json
-git fetch origin 456478b3c50d60100dbb1fb9bc931f370a2c1c28
-git checkout 456478b3c50d60100dbb1fb9bc931f370a2c1c28
 mkdir build && cd build
 cmake -DJSON_BuildTests=false ..
 make -j$(nproc)
